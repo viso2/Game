6775fe78a7d5224ce19e0af6300c482d4d688b1c
3H0lt <Valdemartraeholt@gmail.com>
2025-03-04 09:21:23 +0100

    Working Background
    Baggrunden refresher sig når man bevæger sig til begge sider

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -19,7 +19,7 @@ public class Parallax : MonoBehaviour
             SpriteRenderer spriteRenderer = backgroundLayers[i].GetComponent<SpriteRenderer>();
             if (spriteRenderer != null)
             {
-                layerWidths[i] = spriteRenderer.bounds.size.x;
+                layerWidths[i] = 21.3f/*spriteRenderer.bounds.size.x*/;
             }
         }
     }
@@ -38,7 +38,7 @@ public class Parallax : MonoBehaviour
             {
                 // Reposition the background layer to the end of the sequence
                 Vector3 newPosition = backgroundLayers[i].position;
-                newPosition.x += layerWidths[i] * backgroundLayers.Length;
+                newPosition.x += layerWidths[i] * backgroundLayers.Length/5;
                 backgroundLayers[i].position = newPosition;
             }
             // Check if the background layer has exited the screen to the left
@@ -46,7 +46,7 @@ public class Parallax : MonoBehaviour
             {
                 // Reposition the background layer to the start of the sequence
                 Vector3 newPosition = backgroundLayers[i].position;
-                newPosition.x -= layerWidths[i] * backgroundLayers.Length;
+                newPosition.x -= layerWidths[i] * backgroundLayers.Length/5;
                 backgroundLayers[i].position = newPosition;
             }
         }
