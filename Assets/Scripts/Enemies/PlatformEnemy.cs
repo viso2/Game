8763e964ae5c3739323cd0315a4058ad26e920eb
viso2 <145560317+viso2@users.noreply.github.com>
2025-03-05 10:29:16 +0100

    Refactored Enemies
    Refactored enemies to better change the overall system for building enemies in the game. All enemies should inherit the EnemyBaseClass

--- Assets/Scripts/Enemies/PlatformEnemy.cs
+++ Assets/Scripts/Enemies/PlatformEnemy.cs
@@ -1,54 +1,22 @@
 using UnityEngine;
 
-public class PlatformEnemy : MonoBehaviour
+public class PlatformEnemy : EnemyBase
 {
-    public float speed = 2f;
-    public float detectionRange = 5f;
-    public float jumpForce = 5f;
-    public Transform player;
-    private Rigidbody2D rb;
-    private Animator animator;
-    private bool isOnPlatform;
-    public LayerMask platformLayer;
-    private BoxCollider2D boxCollider; 
-    private bool facingRight = true;
 
-    void Start()
+    [SerializeField] private float jumpForce = 5f;
+    private BoxCollider2D boxCollider;
+    private bool isOnPlatform;
+
+    protected override void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        base.Start();
         boxCollider = GetComponent<BoxCollider2D>();
-        rb.freezeRotation = true;
     }
 
-    void Update()
+    protected override void Update()
     {
         CheckIfOnPlatform();
-
-        if (player != null && isOnPlatform) {
-            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
-            if (distanceToPlayer < detectionRange) {
-
-                if (Mathf.Abs(player.position.y-transform.position.y) > 1)
-                    JumpToPlayer();
-                else
-                    ChasePlayer();
-
-            } else {
-                rb.linearVelocity = Vector2.zero;
-            }
-        }    
-    }
-
-    private void ChasePlayer()
-    {
-        Vector2 direction = new Vector2(player.position.x-transform.position.x, 0).normalized;
-        rb.linearVelocity = new Vector2(direction.x * speed, rb.linearVelocityY);
-        if (rb.linearVelocityX < 0 && facingRight)
-            Flip();
-        else if (rb.linearVelocityX > 0 && !facingRight)
-            Flip();
-
+        base.Update();
     }
 
     private void JumpToPlayer()
@@ -57,16 +25,23 @@ public class PlatformEnemy : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
     }
 
+    protected override void MoveTowardsPlayer()
+    {
+        if (!isOnPlatform) return;
+
+        float verticalDifference = Mathf.Abs(player.position.y - transform.position.y);
+        if (verticalDifference > 1) JumpToPlayer();
+        else ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        Vector2 direction = new Vector2(player.position.x - transform.position.x, 0).normalized;
+        rb.linearVelocity = new Vector2(direction.x * speed, rb.linearVelocityY);
+    }
+
     private void CheckIfOnPlatform()
     {
         isOnPlatform = boxCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
     }
-
-    private void Flip ()
-    {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    } 
 }
