e150fea7e7fd56c0e22f527b14e10a0b49821b24
3H0lt <Valdemartraeholt@gmail.com>
2025-05-09 13:02:32 +0200

    Edited main scene adding new assets

--- Assets/Scripts/Gameplay/EnemyBase.cs
+++ Assets/Scripts/Gameplay/EnemyBase.cs
@@ -57,13 +57,13 @@ namespace Gameplay
 
         protected abstract void MoveTowardsPlayer();
 
-        protected virtual void OnCollisionEnter2D(Collision2D collision)
+        /*protected virtual void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.gameObject.CompareTag("Player"))
                 Debug.Log("Hit");
             ChangeState(EnemyState.Attack);
             timeSinceLastAttack = 0;
-        }
+        }*/
 
         public void TakeDamage(float attackDamage)
         {
